4fc0f9a572b7022ffee17360f3ad35155305393a
shubham tiwari <st1173076@gmail.com>
2024-02-15 22:58:07 +0530

    added facebook page connection routes

--- client/src/Components/ConnectFbModal.jsx
+++ client/src/Components/ConnectFbModal.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 
 function ConnectFbModal() {
+
+    const handleFacebookAuth = () => {
+        window.location.href = "https://richpanel-ruby.vercel.app/auth/facebook";
+      };
+
   return (
     <div className="w-80 h-44 bg-white flex flex-col items-center justify-center rounded-md">
         <h3 className='mb-10'>Facebook Page Integration</h3>
-        <button className='bg-[#1d4e91] text-white p-2 w-[80%] mx-4 rounded-md'>Connect Page</button>
+        <button className='bg-[#1d4e91] text-white p-2 w-[80%] mx-4 rounded-md' onClick={handleFacebookAuth}>Connect Page</button>
     </div>
   )
 }
