fdf4cc8df64f2f37aa4fbc8df165c81c081a2bfb
shubham tiwari <st1173076@gmail.com>
2024-02-15 12:17:58 +0530

    added test server endpoint

--- backend/index.js
+++ backend/index.js
@@ -14,6 +14,30 @@ app.use(express.json());
 
 app.use('/', userRoutes)
 
+app.get('/test', (req, res) => {
+   res.json({name:"yeloow"})
+})
+
+app.get("/webhook", (req, res) => {
+    // Parse the query params
+    let mode = req.query["hub.mode"];
+    let token = req.query["hub.verify_token"];
+    let challenge = req.query["hub.challenge"];
+  
+    // Check if a token and mode is in the query string of the request
+    if (mode && token) {
+      // Check the mode and token sent is correct
+      if (mode === "subscribe" && token === config.verifyToken) {
+        // Respond with the challenge token from the request
+        console.log("WEBHOOK_VERIFIED");
+        res.status(200).send(challenge);
+      } else {
+        // Respond with '403 Forbidden' if verify tokens do not match
+        res.sendStatus(403);
+      }
+    }
+  });
+
 app.listen(3001, () => {
   console.log("listening to port 3001")
 })
