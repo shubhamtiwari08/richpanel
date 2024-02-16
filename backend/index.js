8a78c0c685309c8405b445e72694d4655b372948
shubham tiwari <st1173076@gmail.com>
2024-02-17 00:45:37 +0530

    added facebook page connection routes

--- backend/index.js
+++ backend/index.js
@@ -37,12 +37,18 @@ app.post('/webhook', (req, res) => {
     if (body.object === 'page') {
   
       // Iterates over each entry - there may be multiple if batched
-      body.entry.forEach(function(entry) {
+      body.entry.forEach(async function(entry) {
   
         // Gets the message. entry.messaging is an array, but 
         // will only ever contain one message, so we get index 0
         let webhook_event = entry.messaging[0];
         console.log(webhook_event,"check");
+
+        const message = Message({webhook_event})
+
+        const saved_message = await message.save()
+
+
 
 
       });
